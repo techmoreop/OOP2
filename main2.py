d3f0470aed5a1602261fc19beb226abc6549c3df
techmoreop <techmoreop@gmail.com>
2026-03-15 20:56:23 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,11 @@
+class pe:
+    def twosum(self,numbers,target):
+       lookup = {}
+        for i, num in enumerate(num):
+            if target - num in lookup:
+               return (lookup[target-num], i lookup[num] = i
+walue = int(input("enter sum for whichh you want to make this search")
+print("index=%d, index2=%d") %
+pe().twosum((10,20,30,40,50,60,70), value))
+
+        
